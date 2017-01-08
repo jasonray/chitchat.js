faa4e31c531344c172d148f3705f976e5e079231
jason.ray <jasonray@jayray.net>
2017-01-08 17:38:46 -0500

    trying to split client and server

--- server.js
+++ server.js
@@ -2,16 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+var port = 3000;
+http.listen(port, function(){
+    console.log('server running on port:' + port);
 });
