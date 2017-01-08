66ce260f481ef7612b7b4acfe6e875ae3c235e96
Jason Ray <jasonray@jayray.net>
2017-01-08 17:50:41 -0500

    Revert "trying to split client and server"
    This reverts commit faa4e31c531344c172d148f3705f976e5e079231.

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
     });
 });
 
-var port = 3000;
-http.listen(port, function(){
-    console.log('server running on port:' + port);
+http.listen(3000, function(){
+    console.log('listening on *:3000');
 });
